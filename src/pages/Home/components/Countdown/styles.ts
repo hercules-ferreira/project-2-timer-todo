6ae283decb733f57ecf6f3e4bd0ae988ebb5d8d4
Hercules Ferreira <olcaminote82019@gmail.com>
2023-08-19 12:39:44 -0300

    media

--- src/pages/Home/components/Countdown/styles.ts
+++ src/pages/Home/components/Countdown/styles.ts
@@ -15,6 +15,36 @@ span{
     padding: 2rem 1rem;
     border-radius: 8px;
 }
+
+@media (min-width: 768px) and (max-width: 1023px) {
+    font-size: 6rem;
+    line-height: 5rem;
+    
+
+  }
+
+@media (min-width: 481px) and (max-width: 767px) {
+    font-size: 5rem;
+    line-height: 4rem;
+ 
+
+  }
+
+@media (max-width: 480px) {
+    display: flex;
+    font-size: 4.5rem;
+    line-height: 3.5rem;
+    gap: 0.6rem;
+   
+}
+
+@media (max-width: 400px) {
+    font-size: 3.5rem;
+    line-height: 2.5rem;  
+    gap: 0.5rem;
+    
+}
+
 `;
 
 
@@ -26,5 +56,27 @@ overflow: hidden;
 display: flex;
 justify-content: center;
 
+
+@media (min-width: 768px) and (max-width: 1023px) {
+    font-size: 6rem;
+    line-height: 5rem;
+
+  }
+
+@media (min-width: 481px) and (max-width: 767px) {
+    font-size: 5rem;
+    line-height: 4rem;
+  }
+
+@media (max-width: 480px) {
+    font-size: 4.5rem;
+    line-height: 3.5rem; 
+}
+
+@media (max-width: 400px) {
+    font-size: 3.5rem;
+    line-height: 2.5rem;  
+}
 `;
+
 
